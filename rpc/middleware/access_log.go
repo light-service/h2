7bda6952b69b74f9b5c936d6d2e7461514540a12
lianyongliang <lianyongliang@mudu.tv>
2020-09-28 11:37:48 +0800

    grpc

--- rpc/middleware/access_log.go
+++ rpc/middleware/access_log.go
@@ -16,28 +16,25 @@ func NewAccessLogInterceptor(logger log.Interface) grpc.UnaryServerInterceptor {
 
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 		since := time.Now()
+		resp, err = handler(ctx, req)
+
 		remoteIP := ""
+		hostname, _ := os.Hostname()
+		reqJson, _ := json.Marshal(req)
+		elapsed := time.Since(since)
 		if p, ok := peer.FromContext(ctx); ok {
 			remoteIP = p.Addr.String()
 		}
-
-		defer func() {
-			hostname, _ := os.Hostname()
-			elapsed := time.Since(since)
-			reqJson, _ := json.Marshal(req)
-			fieldLogger.WithFields(map[string]interface{}{
-				"host":          hostname,
-				"full_method":   info.FullMethod,
-				"remote_ip":     remoteIP,
-				"request":       reqJson,
-				"code":          status.Code(err),
-				"error":         err,
-				"latency":       int(elapsed),
-				"latency_human": elapsed,
-			}).Info()
-		}()
-
-		resp, err = handler(ctx, req)
+		fieldLogger.WithFields(map[string]interface{}{
+			"host":          hostname,
+			"full_method":   info.FullMethod,
+			"remote_ip":     remoteIP,
+			"request":       string(reqJson),
+			"code":          status.Code(err),
+			"error":         err,
+			"latency":       int(elapsed),
+			"latency_human": elapsed.String(),
+		}).Info()
 		return resp, err
 	}
 }
